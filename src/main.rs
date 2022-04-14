f2b2a0c4e33d0166c6ad97011ec7336e5fbe6427
KirillProgrammer <kirillkarpovprogramm@gmail.com>
2022-04-14 19:17:54 +0300

    simple rust game

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Угадай число!");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Секретное число равно {}", secret_number);
+
+    loop {
+        println!("Введите свою догадку");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Не получилось прочитать строку");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("Вы загадали {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Мало"),
+            Ordering::Greater => println!("Много"),
+            Ordering::Equal => {
+                println!("То самое");
+                break;
+            }
+        }
+    }
 }
     
