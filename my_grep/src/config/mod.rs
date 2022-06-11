9579c040fbcbed8169dc981ad8803804fd858f86
Kirill Karpov <kirillkarpovprogramm@gmail.com>
2022-06-11 13:09:05 +0300

    Добавил возможность поиску не чувствительного к регистру через флаг -ncs а также вывод ошибки именно в stderr

--- my_grep/src/config/mod.rs
+++ my_grep/src/config/mod.rs
@@ -11,6 +11,7 @@ pub mod core {
 	pub struct Config {
 		pub query: String,
 		pub filename: String,
+		pub case_sensitive: bool,
 	}
 
 	impl Config {
@@ -19,17 +20,31 @@ pub mod core {
 				// panic!("Нужно больше аргументов: строка поиска и имя файл")
 				return Err("Нужно больше аргументов: строка поиска и имя файлa");
 			}
+			let mut case_sensitive = true;
+			// = std::env::var("CASE_INSENSITIVE").is_err();
+			for arg in args {
+				if arg == "-ncs" || arg == "--no-case-sensitive" {
+					case_sensitive = false;
+				}
+			}
 			let query = args[1].clone();
 			let filename = args[2].clone();
 
 			// Config { query, filename }
-			Ok(Config { query, filename })
+			Ok(Config { query, filename, case_sensitive })
 		}
 	}
 
 	pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 		let contents = fs::read_to_string(config.filename)?;
-		for line in search(&config.query, &contents) {
+
+		let result = if config.case_sensitive {
+			search(&config.query, &contents)
+		} else {
+			search_case_insensitive(&config.query, &contents)
+		};
+
+		for line in result {
 			println!("{}", line);
 		}
 		Ok(())
@@ -48,6 +63,18 @@ pub mod core {
 			return vec!["Не найдено совпадений"];
 		}
 	}
+	pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+		let query = query.to_lowercase();
+		let mut results = Vec::new();
+
+		for line in contents.lines() {
+			if line.to_lowercase().contains(&query) {
+				results.push(line);
+			}
+		}
+
+		results
+	}
 }
 
 #[cfg(test)]
@@ -57,7 +84,8 @@ mod tests {
 	fn new_config() {
 		let config = Config { 
 			query: String::from("search"), 
-			filename: String::from("b.txt") 
+			filename: String::from("b.txt"),
+			case_sensitive: true,
 		};
 
 		let args = 
@@ -84,16 +112,32 @@ mod tests {
 	}
 
 	#[test]
-	fn one_result() {
+	fn case_sensitive() {
 		let query = "duct";
 		let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.
+Duct tape.
 		";
 		assert_eq!(
 			vec!["safe, fast, productive."],
 			search(query, contents)
 		);
 	}
+	#[test]
+	fn case_insensitive() {
+		let query = "rUsT";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+		assert_eq!(
+			vec!["Rust:", "Trust me."],
+			search_case_insensitive(query, contents)
+		);
+	}
+	
 }
--- my_grep/src/main.rs
+++ my_grep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
 	let args: Vec<String> = env::args().collect();
 	
 	let config = Config::new(&args).unwrap_or_else(|err| {
-		println!("Проблема при разборе аргументов: {}", err);
+		eprintln!("Проблема при разборе аргументов: {}", err);
 		std::process::exit(1);
 	});
 	
 	if let Err(e) = run(config) {
-		println!("Ошибка в приложении: {}", e);
+		eprintln!("Ошибка в приложении: {}", e);
 		std::process::exit(1);
 	};	
 }		
