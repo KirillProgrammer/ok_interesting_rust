7cbe97db03c1bb92e9ccd6bc0019e764d416747e
Kirill Karpov <kirillkarpovprogramm@gmail.com>
2022-06-12 17:03:15 +0300

    итак, смог сделать так, чтобы и со строками работало, теперь выглядит более удовлетворительно

--- closures_and_iterators/src/lib.rs
+++ closures_and_iterators/src/lib.rs
@@ -10,7 +10,7 @@ struct Cacher<T, V>
 impl<T, V> Cacher<T, V>
 	where T: Fn(V) -> V,
 	V: Eq + std::hash::Hash,
-	V: Eq + std::hash::Hash + Copy
+	V: Eq + std::hash::Hash + Clone
 {
 	fn new(calculation: T) -> Cacher<T, V> {
 		Cacher {
@@ -20,16 +20,10 @@ impl<T, V> Cacher<T, V>
 	}
 	
 	fn value(&mut self, arg: V) -> V {
-		let kar = self.value.get(&arg);
-		match kar {
-			Some(k) => *k,
-			None => {
-				let v = (self.calculation)(arg);
-				self.value.insert(v, v);
-				v
-			},
-		}
-
+		let key = arg.clone();
+		let func_return = arg.clone();
+		self.value.entry(key).or_insert(arg);
+		func_return
 	}
 }
 
@@ -37,7 +31,7 @@ impl<T, V> Cacher<T, V>
 mod tests {
 	use super::*;
 	#[test]
-	fn call_with_dif_val() {
+	fn cache_nums() {
 		let mut cache = Cacher::new(|a| a);
 
 		let v1 = cache.value(1);
@@ -46,16 +40,16 @@ mod tests {
 		assert_ne!(v1, v2);
 		assert_eq!(v2, 2);
 	}
-	// #[test]
-	// fn cache_strings() {
-	// 	let mut cache = Cacher::new(|a| a);
+	#[test]
+	fn cache_strings() {
+		let mut cache = Cacher::new(|a| a);
 
-	// 	let v1 = cache.value("hello".to_string());
-	// 	let v2 = cache.value("bye".to_string());
+		let v1 = cache.value("hello".to_string());
+		let v2 = cache.value("bye".to_string());
 
-	// 	assert_ne!(v1, v2);
-	// 	assert_eq!(v2, "bye".to_string());
-	// }
+		assert_ne!(v1, v2);
+		assert_eq!(v2, "bye".to_string());
+	}
 	#[test]
 	fn cache_str() {
 		let mut cache = Cacher::new(|a| a);
@@ -66,14 +60,4 @@ mod tests {
 		assert_ne!(v1, v2);
 		assert_eq!(v2, "bye");
 	}
-	// #[test]
-	// fn cache_float() {
-	// 	let mut cache = Cacher::new(|a| a);
-
-	// 	let v1 = cache.value(123.32);
-	// 	let v2 = cache.value(2.31);
-
-	// 	assert_ne!(v1, v2);
-	// 	assert_eq!(v2, 2);
-	// }
 }
