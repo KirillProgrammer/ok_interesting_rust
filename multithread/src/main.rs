4bfe0a37a515f6564a0111a92f8167643a2dd09c
KirillProgrammer <kirillkarpovprogramm@gmail.com>
2022-07-19 13:01:12 +0300

    многопоточность дело такое, только теперь еще мьютексы, без каналов связи

--- multithread/src/main.rs
+++ multithread/src/main.rs
@@ -1,34 +1,24 @@
-use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let tx1 = mpsc::Sender::clone(&tx);
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("Hellof"),
-            String::from("from"),
-            String::from("thread"),
-        ];
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_millis(10));
-        }
-    });
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("message"),
-            String::from("for"),
-            String::from("you"),
-        ];
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_millis(15));
-        }
-    });
-    for received in rx {
-        println!("I get: {:?}", received);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+            println!("Thread number: {i} has value: {}", *num);
+        });
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
